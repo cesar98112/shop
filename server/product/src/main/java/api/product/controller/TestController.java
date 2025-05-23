8ff83780fdcf1fc5e3b0ca57efebd752d686dd08
cesar jesus leon martin <cesarjml98@gmail.com>
2025-05-23 17:20:55 +0200

    feature: implement actuator refresh

---
+++ server/product/src/main/java/api/product/controller/TestController.java
@@ -0,0 +1,21 @@
+package api.product.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/actuator")
+@RefreshScope
+public class TestController {
+
+    @Value("${app.prop}")
+    private String prop;
+
+    @GetMapping("/getProp")
+    public String getProp(){
+        return this.prop;
+    }
+}
